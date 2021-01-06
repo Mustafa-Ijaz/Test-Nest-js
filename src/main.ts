54fc81690996053e176532ab37eb89fe831b3d4f
mustafaijaz <mustafa.ijaz@redbuffer.net>
2021-01-06 15:20:12 +0500

    added nest js swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.setGlobalPrefix('api');
+
+  const documentDetails = new DocumentBuilder()
+  .setTitle('Testing api')
+  .setDescription('Testing the Nest js swagger')
+  .build()
+  const document = SwaggerModule.createDocument(app,documentDetails);
+  SwaggerModule.setup('api',app,document);
+
+  await app.listen(3000); 
 }
 bootstrap();
--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,19 +1,29 @@
 import { Controller,Post,Body,Get } from "@nestjs/common";
 
 import { ProductsService } from "./products.service";
+import { ApiCreatedResponse } from '@nestjs/swagger';
 
 @Controller('products')
 export class ProductsController{
     constructor (private readonly productsService: ProductsService){}
+
     @Post()
+    @ApiCreatedResponse({
+        status : 201,
+        description : "Item Successfully Added to Database"
+    })
     viewProducts(
-        @Body('title') proTitle : string
-        ): any {
-        return this.productsService.insertTitle(proTitle);
+        @Body('title') productTitle : string,
+        @Body('price') productPrice : number
+        ) {
+        return this.productsService.insertTitle(productTitle,productPrice);
     }
     @Get('test')
-    test() {
-        console.log("Hello")
+    @ApiCreatedResponse({
+        status : 201,
+        description : "Server is running successfully"
+    })
+    test() : string {
         return "Test Successfully completed"
     }
 
